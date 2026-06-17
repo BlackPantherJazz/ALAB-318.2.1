2f5c17494281cfbffbbe8d20d430ce456e6b0d91
BlackPantherJazz <henocmontes29@gmail.com>
2026-06-16 21:49:38 -0400

    initial project setup with express and ejs configured

---
+++ index.js
@@ -0,0 +1,23 @@
+import express, { Route } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', ejs)
+
+// Middleware to parse
+app.use(express.urlencoded({extended:true}));
+
+// Serve static files from the publuc folder
+app.use(express.static('public'));
+
+//Home route
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+
+});
